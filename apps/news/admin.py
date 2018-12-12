4b668e12c38652cd09a93e63cb47269a3c1e4d01
vimm0 <vimmrana0@gmail.com>
2018-12-12 21:43:32 +0545

    fix news

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminMixin
 
-from apps.news.models import News
+from apps.news.models import News, Category, Reporter, Publication
 
 
 # class NewsAdmin(admin.ModelAdmin):
 #     list_display = ('title',)
+@admin.register(Category)
+class CategoryAdmin(SortableAdminMixin, admin.ModelAdmin):
+    pass
 
 
+# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Reporter)
+admin.site.register(Publication)
 admin.site.register(News)
--- apps/news/models.py
+++ apps/news/models.py
@@ -2,6 +2,49 @@ from django.db import models
 from froala_editor.fields import FroalaField
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    description = FroalaField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
 class News(models.Model):
     title = models.CharField(max_length=255, blank=True, null=True)
     content = FroalaField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    publications = models.ManyToManyField(Publication)
+    category = models.ManyToManyField(Category)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
